518f4c15fb798428626128d932c7a6ed5cb1f5ca
gnaroshi <gnaroshi@gmail.com>
2024-10-02 16:08:47 +0900

    20241002 [UPDATE] vision x homepage test

---
+++ vision_x/src/components/tabs/Home/NewsCard.jsx
@@ -0,0 +1,17 @@
+import "./NewsCard.css";
+
+function NewsCard() {
+  return (
+    <div className="news-card">
+      <div className="news-card-date-wrapper">
+        <p className="news-card-date">2024/09/28</p>
+      </div>
+      <div className="news-card-content">
+        <h1>WOW</h1>
+        <p>fdsafdsafdsaasdf</p>
+      </div>
+    </div>
+  );
+}
+
+export default NewsCard;
---
+++ vision_x/src/components/tabs/Join.jsx
@@ -0,0 +1,12 @@
+import joinImage from "../../assets/IMG_0148.jpeg";
+import "./Join.css";
+function Join() {
+  return (
+    <div id="join">
+      <h1>Join</h1>
+      <img src={joinImage} alt="" />
+    </div>
+  );
+}
+
+export default Join;
